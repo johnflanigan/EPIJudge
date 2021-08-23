f4866f503f951c8a56ad409da4b7d0ee49eb3217
John Flanigan <johnrflanigan@gmail.com>
2021-08-23 16:21:14 -0400

    Implement HIndex

--- epi_judge_java/epi/HIndex.java
+++ epi_judge_java/epi/HIndex.java
@@ -1,20 +1,34 @@
 package epi;
+
+import java.util.Comparator;
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
 public class HIndex {
   @EpiTest(testDataFile = "h_index.tsv")
   public static int hIndex(List<Integer> citations) {
-    // TODO - you fill in here.
-    return 0;
+    citations.sort(Comparator.naturalOrder());
+
+    int hValue = 0;
+
+    for (int i = 0; i < citations.size(); i++) {
+      int count = citations.size() - i;
+
+      int h = Integer.min(count, citations.get(i));
+
+      hValue = Integer.max(hValue, h);
+    }
+
+    return hValue;
   }
 
   public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "HIndex.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -1644,7 +1644,7 @@ problem_mapping = {
                 "total": 1001
             },
             "Java: HIndex.java": {
-                "passed": 0,
+                "passed": 1001,
                 "total": 1001
             },
             "Python: h_index.py": {
